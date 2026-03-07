5b77823ec04c12c787df9a1abc72a92483d35282
turnclay <clayton.malin24@gmail.com>
2026-03-07 02:10:07 +0100

    Auto update

--- auto-push.ps1
+++ auto-push.ps1
@@ -1,6 +1,6 @@
 # --- CONFIGURATION ---
 $projectPath = "C:\Users\clayt\PycharmProjects\MiniGameV3"
-$renderServiceId = "srv-d6kv2u5m5p6"
+$renderServiceId = "srv-d6kv2u5m5p6s7389veag"
 $renderApiKey = "rnd_h5AV4uaXG6HYErpoqf0y5fbVQQIU"
 
 # --- SCRIPT ---
